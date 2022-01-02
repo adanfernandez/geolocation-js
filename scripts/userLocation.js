620afdfe3a8c19d285ec313c4882f8fc6decb95c
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2022-01-02 23:48:22 +0100

    Treasure added

--- scripts/userLocation.js
+++ scripts/userLocation.js
@@ -1,5 +1,5 @@
 if (navigator.geolocation) {
-	var id=navigator.geolocation.watchPosition(procesarPosicion, fallo);
+	navigator.geolocation.getCurrentPosition(procesarPosicion, fallo);
 } else {
 	window.alert("Tu navegador no permite la geolocalización.");
 }
@@ -7,16 +7,18 @@ if (navigator.geolocation) {
 function procesarPosicion(position) {
 	var ltd=position.coords.latitude;
 	var lng=position.coords.longitude;
-
+	
 	var exactitud=position.coords.accuracy;
 	var timestamp=position.timestamp;
+
 	L.mapbox.accessToken = config.SECRET_API_KEY_MAPBOX;
-
+	
 	var bounds = calcBounds(ltd, lng);
-
 	var map = L.mapbox.map('map').setView([ltd, lng], 9).addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+	
 	L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(map);
 	map.fitBounds(bounds);
+	const marker = placeTreasure(bounds[0][0], bounds[0][1], map);
 }
 
 function fallo() {
@@ -37,4 +39,14 @@ function calcSouthEastCorner(ltd, lng) {
 	var new_ltd = ltd - (radius / r_earth) * (180 / pi);
 	var new_lng = lng + (radius / r_earth) * (180 / pi) / Math.cos(new_ltd * pi / 180);
 	return [new_ltd, new_lng];
+}
+
+function placeTreasure(lat, lng, map) {
+	const ltd_variation = Math.floor(Math.random() * side + 1) / 1000;
+	const lng_variation = Math.floor(Math.random() * side + 1) / 1000;
+
+	var new_ltd = lat- (ltd_variation / r_earth) * (180 / pi);
+	var new_lng = lng + (lng_variation / r_earth) * (180 / pi) / Math.cos(new_ltd * pi / 180);
+
+	return L.marker([new_ltd, new_lng]).addTo(map);
 }

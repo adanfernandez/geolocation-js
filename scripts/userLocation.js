82f7679c0e8ecd9c7570514fc344f746c90afd79
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2022-01-02 22:33:39 +0100

    Square drawn

--- scripts/userLocation.js
+++ scripts/userLocation.js
@@ -5,20 +5,36 @@ if (navigator.geolocation) {
 }
 
 function procesarPosicion(position) {
-	var latitud=position.coords.latitude;
-	var longitud=position.coords.longitude;
+	var ltd=position.coords.latitude;
+	var lng=position.coords.longitude;
+
 	var exactitud=position.coords.accuracy;
 	var timestamp=position.timestamp;
+	L.mapbox.accessToken = config.SECRET_API_KEY_MAPBOX;
 
-	var panel=document.getElementById("panel");
+	var bounds = calcBounds(ltd, lng);
 
-	var mensaje="<p>"+latitud+", "+longitud+" (Error: "+exactitud+"m)<br>";
-	
-	mensaje+=timestamp+"</p>";
-
-	panel.innerHTML+=mensaje;
+	var map = L.mapbox.map('map').setView([ltd, lng], 9).addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+	L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(map);
+	map.fitBounds(bounds);
 }
 
 function fallo() {
 	window.alert("No es posible geolocalizarte. Comprueba que tienes activada esa posibilidad.");
+}
+
+function calcBounds(ltd, lng) {
+	return [calcNortWesthCorner(ltd, lng), calcSouthEastCorner(ltd, lng)];
+}
+
+function calcNortWesthCorner(ltd, lng) {	
+    var new_ltd = ltd + (radius / r_earth) * (180 / pi);
+	var new_lng = lng - (radius / r_earth) * (180 / pi) / Math.cos(new_ltd * pi / 180);
+	return [new_ltd, new_lng];
+}
+
+function calcSouthEastCorner(ltd, lng) {
+	var new_ltd = ltd - (radius / r_earth) * (180 / pi);
+	var new_lng = lng + (radius / r_earth) * (180 / pi) / Math.cos(new_ltd * pi / 180);
+	return [new_ltd, new_lng];
 }
